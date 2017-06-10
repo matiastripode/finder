48834828da82197d1caf90445136b650015312ea
nicolas.porpiglia@globant.com <nicolas.porpiglia@globant.com>
2017-06-10 11:58:39 -0400

    Added initial app flow

--- Finder/Finder/FinderViewController.swift
+++ Finder/Finder/FinderViewController.swift
@@ -10,10 +10,21 @@ import UIKit
 
 class FinderViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var emptyView: UIView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if (true){ //Do we have contacts already?
+            self.tableView.isHidden = true
+            self.emptyView.isHidden = false
+        } else {
+            self.tableView.isHidden = false
+            self.emptyView.isHidden = true
+
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +32,16 @@ class FinderViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
+        return cell
+    }
+
 
 }
