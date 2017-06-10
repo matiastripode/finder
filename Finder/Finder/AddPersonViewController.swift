438a26bfec59b5923d7a491f585863a59344177d
nicolas.porpiglia@globant.com <nicolas.porpiglia@globant.com>
2017-06-10 13:02:48 -0400

    Added flow for adding a new family member

--- Finder/Finder/AddPersonViewController.swift
+++ Finder/Finder/AddPersonViewController.swift
@@ -8,28 +8,101 @@
 
 import UIKit
 
-class AddPersonViewController: UIViewController {
+class AddPersonViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+
+    let imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
     }
     
+    @IBAction func openCameraButton(sender: AnyObject) {
+        
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) &&
+            UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            
+            let alertController = UIAlertController(title: "Source", message: "Select a source", preferredStyle: .alert)
+            
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { action in
+                self.imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+                self.present(self.imagePicker, animated: true, completion: nil)
+            }
+            alertController.addAction(cameraAction)
+            
+            let libraryAction = UIAlertAction(title: "Photo library", style: .default) { action in
+                self.imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary;
+                self.present(self.imagePicker, animated: true, completion: nil)
+            }
+            alertController.addAction(libraryAction)
+            
+            self.present(alertController, animated: true) {
+                
+            }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        }
     }
-    */
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        imageView.image = image
+        self.dismiss(animated: true, completion: nil);
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.dismissKeyboard()
+        return true
+    }
+    
+    @IBAction func dismissKeyboard() {
+        self.textField.resignFirstResponder()
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let pickedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func onAddPerson () {
+        if let text = self.textField.text, let image = self.imageView.image, text != "" {
+            let familyMember = FamilyMember (image: image, name:text)
+            
+            //TODO: Add family member to user
+            self.dismiss(animated: true, completion: nil)
+        } else {
+            
+            let alertController = UIAlertController(title: "Error", message: "Image and name are required", preferredStyle: .alert)
+            
+            let cameraAction = UIAlertAction(title: "Ok", style: .cancel) { action in
+            }
+            alertController.addAction(cameraAction)
+            
+            self.present(alertController, animated: true) {
+            }
+        }
+    }
+    
+    
 
 }
